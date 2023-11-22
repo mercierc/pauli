e30c056544446383596c00eb1af4916a9db6162a
mercierc <clementmercier0@outlook.fr>
2023-11-22 20:35:48 +0100

    Add pauli.sh raw shell file;

---
+++ data/pauli.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# User defined functions.
+
+function preinstall(){
+    echo "preinstall not implemented."
+}
+
+function build(){
+    echo "build not implemented."
+}
+
+function run(){
+    echo run
+    docker run alpine ls
+    # docker run -ti influxdb:2.7 sh
+}
+
+function clean(){
+    echo "clean not implemented."
+}
+
+function lint(){
+    echo "lint not implemented."
+}
+
+function unittests(){
+    echo "unittests not implemented."
+}
+
+function inttests(){
+    echo "inttests not implemented."
+}
+
+# Common functions
+function fatal(){
+	echo -e "\e[1;4;97;48;5;9mFATAL: $1 \e[0m"
+}
+function highlight(){
+	echo -e "\n\e[1;92m $1 \e[0m\n"
+}
+
+function info(){
+	echo -e "\e[92m $1 \e[0m"
+}
+
+function warn(){
+	echo -e "\e[38;5;208m $1 \e[0m"
+}
+
+preinstall
+
+case $1 in
+	build)
+		build ;;
+	run)
+	        run ;;
+	unittests)
+		unittests "${@:2}" ;;
+	lint)
+		lint ;;
+	inttests)
+		inttests ;;
+	staticanalysis)
+		staticanalysis "${@:2}" ;;
+	*)
+		fatal "Unknown command"
+		exit 1;;
+esac
