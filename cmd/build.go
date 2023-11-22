4fa737cd45a5477e0e97c72480fa89d30f8a9dd7
mercierc <clementmercier0@outlook.fr>
2023-11-22 22:42:12 +0100

    initiate build command.

---
+++ cmd/build.go
@@ -0,0 +1,19 @@
+package cmd
+
+import(
+	
+	"github.com/spf13/cobra"
+
+	"github.com/mercierc/pauli/src"
+)
+
+
+var buildCmd = &cobra.Command{
+	Use: "build",
+	Short: "Execute the build from pauli.sh",
+	Long: "Launch a build container and execute the build function " +
+		"pauli.sh from it.",
+	Run: func(cmd *cobra.Command, arg []string){
+		src.InitiateProject()
+	},
+}
