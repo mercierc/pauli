1a770ee9691b18c1a1df65ea9af728e941b7a1dd
mercierc <clementmercier0@outlook.fr>
2024-02-17 14:54:00 +0100

    test container manager creation.

---
+++ src/container_manager_test.go
@@ -0,0 +1,108 @@
+package src
+
+import(
+	"testing"
+	"io/ioutil"
+	"gopkg.in/yaml.v3"
+	"bufio"
+	"fmt"
+	"os"
+	"github.com/mercierc/pauli/logs"
+)
+
+
+
+// This test the most important Opt. It creates a container from the config.yaml
+// file and test that the created container is well configured.
+func  TestNewContainerManager(t *testing.T) {
+
+	logs.Init("trace", true)
+	config := Configuration{
+		Builder: Builder{
+			Image: `hello-world`,
+			Tag: `latest`,
+			Privileged: true,
+			Volumes: []Volume{
+				Volume{
+					Type: `bind`,
+					Source: `/var/run/docker.sock`,
+					Target: `/var/run/docker.sock`,
+				},
+			},
+			
+		},
+		Name: `Test_pauli_image`,
+	}
+
+	yamlData, err := yaml.Marshal(&config)
+	if err != nil {
+		t.Fatalf("Error at yaml writing: %v", err)
+	}
+
+	file, err := ioutil.TempFile("/tmp", "config.yaml")
+	writer := bufio.NewWriter(file)
+
+	_, err = writer.WriteString(string(yamlData))
+	writer.Flush()
+
+	defer file.Close()
+	defer os.Remove(file.Name())
+
+	cm := NewContainerManager(
+		WithName("pauli_TI"),
+		WithEnv([]string{"VAR1=1", "VAR2=2"}),
+		WithConfigYaml(file.Name(), false),
+		WithCmd(append([]string{"/bin/sh", ".pauli/pauli.sh"})),
+	)
+
+
+	// Here we load the information of the created container and ensure
+	// that the config is correct
+	json, err := cm.cli.ContainerInspect(cm.ctx, cm.containerName)
+
+	fmt.Printf("json: %+v", json)
+	fmt.Printf("Mounts %+v", json.Mounts)
+	// Check mounted volumes
+	cwd, err := os.Getwd()
+
+	// Current working dir is mounted by default.
+	mountPoints := []map[string]string{
+		map[string]string{
+			`Source`:  `/var/run/docker.sock`,
+			`Destination`: `/var/run/docker.sock`,
+		},
+		map[string]string{
+			`Source`:  cwd,
+			`Destination`: `/app`,
+		},
+
+	}
+
+	var mountChecked uint8 = 0
+	for _, el := range mountPoints {
+		for _, el2 := range json.Mounts {
+			if el2.Destination == el["Destination"] && el2.Source == el["Source"] {
+				mountChecked += 1
+			}
+		}
+	}
+	fmt.Println("mountedCheck", mountChecked)
+	if mountChecked != uint8(len(mountPoints)) {
+		t.Fatalf("All volumes are not mounted %d/%d", mountChecked, len(mountPoints))
+	} 
+		
+	// Check env variables
+	if json.Config.Env[0] != "VAR1=1" || json.Config.Env[1] != "VAR2=2" {
+	 	t.Fatalf("Wrong env variables %v, %v. Waited: %v %v", json.Config.Env[0], json.Config.Env[1], "VAR1=1", "VAR2=2")
+		
+	}
+
+	// Check the Cmd
+	if json.Config.Cmd[0] != "sleep" || json.Config.Cmd[1] != "infinity" {
+	 	t.Fatalf("Wrong command %v. Waited `sleep infinity`", json.Config.Cmd)
+	}
+			
+	fmt.Println("Cmd ", json.Config.Cmd)
+	
+	
+}
